947449190d3d923013562463113556d6b892e4f1
Charmi Kadi <68164274+charmikadi@users.noreply.github.com>
2021-06-28 14:34:12 -0400

    Create Basics.py

---
+++ Basics.py
@@ -0,0 +1,7 @@
+import math 
+
+radius = 3
+area = math.pi*radius**2 #using the area of a circle formula
+circum = 2*math.pi*radius
+
+print("A circle with radius", radius, "cm has an area of", round(area,1), "square centimetres and a circumference of", round(circum,1), "cm.")  
